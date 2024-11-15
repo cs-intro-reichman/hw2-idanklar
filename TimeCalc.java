102fd4ef0f9ef64997b6f0ca917c58a9069ee9bf
idan klar <idanklar@idans-MacBook-Pro.local>
2024-11-15 17:12:23 +0200

    timecalc check

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,20 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String time = args[0];
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int hours = Integer.parseInt(time.charAt(0) + "" + time.charAt(1));
+        int minutes = Integer.parseInt(time.charAt(3) + "" + time.charAt(4));
+        int totalMinutes = (hours * 60) + minutes + minutesToAdd;
+        int totalHours = totalMinutes / 60;
+        int newHours = totalHours % 24;
+        int newMinutes = totalMinutes - (totalHours * 60);
+        // //System.out.println("hours: " + hours + " minutes: " + minutes + " min to add: " + minutesToAdd + 
+        //                     " new hours: " + newHours + " new minutes: " + newMinutes);
+        if (hours < 10) {
+            System.out.printf("%01d:%02d %n", newHours, newMinutes);
+        }
+        
+        
+        
     }
 }
