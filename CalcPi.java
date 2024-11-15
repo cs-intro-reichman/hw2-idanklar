368fb5bd4f918351277caab39cb0a45b7079eacc
idan klar <idanklar@idans-MacBook-Pro.local>
2024-11-15 22:22:54 +0200

    calcpai1

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,16 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    
+	    int calcNums = Integer.parseInt(args[0]);
+		calcNums = (calcNums * 2) - 1; 
+		double approxPai = 1;
+		boolean isSubtraction = true;
+		for(int i = 3; i <= calcNums ; i = i + 2) {
+			if (isSubtraction) approxPai -= 1 / (double)i;
+			else approxPai += 1 / (double)i;
+			isSubtraction = !isSubtraction;	
+		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + (approxPai * 4));
 	}
 }
